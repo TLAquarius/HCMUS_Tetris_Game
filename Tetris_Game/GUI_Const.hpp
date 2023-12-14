04b247f246b46deda3fd33b611f6c36b21fd6341
fuzziouzz <124992278+fuzziouzz@users.noreply.github.com>
2023-12-14 22:35:12 +0700

    Update GUI_Const.hpp

--- Tetris_Game/GUI_Const.hpp
+++ Tetris_Game/GUI_Const.hpp
@@ -4,8 +4,8 @@
 const int gameOffsetX = 38 * 6;
 const int gameOffsetY = 38;
 const int cellSize = 38;
-const int scoreOffsetX = 0;
-const int scoreOffsetY = 0;
+const int scoreOffsetX = 650;
+const int scoreOffsetY = 100;
 const int nextOffsetX = 0;
 const int nextOffsetY = 0;
 const int holdOffsetX = 0;
